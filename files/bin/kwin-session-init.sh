ad797345b0d5a73d2ac3b06d986dd2b2169b704e
Alexander Heinrich <alxhnr@users.noreply.github.com>
2025-05-21 09:11:49 +0200

    Replace sleep with event

--- files/bin/kwin-session-init.sh
+++ files/bin/kwin-session-init.sh
@@ -1,9 +1,10 @@
 #!/bin/sh -e
 
 sync-guest-with-host.sh &
-(
-  sleep 2.5
-  kscreen-doctor output.Virtual-1.mode.1 output.Virtual-1.scale.1.2
-) &
+
+udevadm monitor -u -s drm |
+stdbuf -o 0 grep -m 1 change |
+xargs -I {} kscreen-doctor output.Virtual-1.mode.1 output.Virtual-1.scale.1.2 &
+
 wbg /usr/local/share/wallpapers/generated.svg &
 xargs -I {} < /tmp/host-serial-output sh -c 'exec {} >/dev/null 2>&1 &' &
