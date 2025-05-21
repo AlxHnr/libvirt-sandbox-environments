18dbb5769952cdeb345206f1762688cdd95598a5
Alexander Heinrich <alxhnr@users.noreply.github.com>
2025-05-21 08:44:02 +0200

    Migrate to wayland
    This replaces Openbox with the KWin compositor, which runs standalone
    without KDE. More lightweight compositors are not usable due to a bug in
    qemu which breaks mouse cursor rendering:
    
    * https://gitlab.com/qemu-project/qemu/-/issues/2315
    
    Weston, Mutter and KWin are the only compositors which work around this
    qemu bug. Weston is the lightest of them, but can't switch output modes
    at runtime. The latter one is required to make VM windows resizable:
    
    * https://gitlab.freedesktop.org/wayland/weston/-/issues/339
    * https://gitlab.freedesktop.org/wayland/weston/-/issues/341
    
    Mutter seems to have hidden hard dependencies on GNOME, which breaks
    certain features like keyboard layout switching, IBus, etc.
    
    This leaves KWin as the only viable option.

---
+++ files/bin/kwin-session.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+
+. /etc/profile
+export PS1='\W $ '
+export FLATPAK_USER_DIR="/var/lib/$USER/flatpak"
+test ! -e "$HOME/.local/bin" || export PATH="$PATH:$HOME/.local/bin"
+test ! -e "/var/lib/$USER/pip3-target" ||
+  export PATH="$PATH:/var/lib/$USER/pip3-target/bin" PYTHONPATH="/var/lib/$USER/pip3-target"
+
+test -e "$HOME/.config" || {
+  kwriteconfig6 --file kxkbrc --group Layout --key LayoutList us
+}
+
+exec dbus-launch /usr/bin/kwin_wayland_wrapper \
+  --no-lockscreen --no-kactivities --xwayland kwin-session-init.sh
