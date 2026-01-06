6a1426c4e34fe98ddf8031e35df368c3093d07b6
Alexander Heinrich <alxhnr@users.noreply.github.com>
2026-01-06 14:13:27 +0100

    Upgrade Alpine to 3.23

--- setup-vms.sh
+++ setup-vms.sh
@@ -688,7 +688,7 @@ test -z "$SETUP_VMS_SH_DONT_RUN" || return 0
 
 cd "$(dirname "$0")"
 
-alpine_version="3.22.1"
+alpine_version="3.23.2"
 vm_data_mountpoint="/vm-data"
 alpine_iso="$vm_data_mountpoint/alpine-standard-$alpine_version-x86_64.iso"
 export LIBVIRT_DEFAULT_URI='qemu:///system'
